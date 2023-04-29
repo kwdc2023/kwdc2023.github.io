568ebd1f38a2a2d222e6ae32a48c12a98b57aa3b
kwdc2023 <132129077+kwdc2023@users.noreply.github.com>
2023-04-29 19:46:31 +0900

    feat: 네비 바 두번 클릭하면 뒤로가게 수정 및 로고 클릭시 홈화면 가게 수정

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
+let kwdclogoButton = document.querySelector(".kwdc-logo");
 let conductButton = document.querySelector(".conduct");
 let termsButton = document.querySelector(".terms");
 
+kwdclogoButton.addEventListener("click", (e) => {
+  location.href = "/index.html";
+});
+
 conductButton.addEventListener("click", (e) => {
-  location.href = "/conduct.html";
+  if (e.target.className.indexOf("active") > 0) {
+    location.href = "/index.html";
+  } else {
+    console.log(e.target.className);
+    location.href = "/conduct.html";
+  }
 });
 
 termsButton.addEventListener("click", (e) => {
-  location.href = "/terms.html";
+  if (e.target.className.indexOf("active") > 0) {
+    location.href = "/index.html";
+  } else {
+    console.log(e.target.className);
+    location.href = "/terms.html";
+  }
 });
